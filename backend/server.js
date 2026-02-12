d9bc88299ecc6d62f5bd05053b8c517fd9547209
gowthamvetri <gowthamvetriii@gmail.com>
2026-02-12 12:04:28 +0530

    deploy fix-2

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,41 @@
 import express from "express"
 import dotenv from "dotenv"
-import db from "./config/db.config.js"
-import authRoutes from "./routes/auth.routes.js"
-import adminRoutes from "./routes/admin.routes.js"
-import userRoutes from "./routes/user.routes.js"
-import paymentRoutes from "./routes/payment.routes.js"
-import { handleWebhook } from "./controller/payment.controller.js"
+import db from "../config/db.config.js"
+import authRoutes from "../routes/auth.routes.js"
+import adminRoutes from "../routes/admin.routes.js"
+import userRoutes from "../routes/user.routes.js"
+import paymentRoutes from "../routes/payment.routes.js"
+import { handleWebhook } from "../controller/payment.controller.js"
 import cors from "cors"
 
-db()
 dotenv.config()
+db()
 
 const app = express()
 
-// Webhook route must be before express.json() to get raw body
 app.post('/api/webhook', express.raw({ type: 'application/json' }), handleWebhook);
 
 app.use(express.json())
-app.use(cors([
-    "http://localhost:5173/**",
-    "https://park-to-go.vercel.app/**"
-]))
 
-app.use('/api',authRoutes);
-app.use("/api",adminRoutes);
-app.use("/api",userRoutes);
-app.use("/api",paymentRoutes);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://park-to-go.vercel.app"
+  ],
+  credentials: true
+}));
 
+app.use('/api', authRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", userRoutes);
+app.use("/api", paymentRoutes);
 
-
-app.get("/",(req,res)=>{
+app.get("/", (req,res)=>{
     return res.status(200).json({
         message:"Hello this is information from server"
     })
 })
 
+app.listen(8080,()=>{ console.log("Serevr started at port 8080") })
 
-app.listen(8080,()=>{
-    console.log("Serevr started at port 8080")
-})
+export default app
