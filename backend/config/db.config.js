872c8a4d99cba53a0f6b78d1df4a136fdfdc6d3b
gowthamvetri <gowthamvetriii@gmail.com>
2026-02-12 12:17:58 +0530

    deploy fix-6

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const db = mongoose.connect(process.env.MONGO_URI).then(()=>{
+const db =async()=>{ 
+    await mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("DB connected sucessfully")
 }).catch(error=>{
     console.log("Error in connecting DB : "+error)
 })
+}
 
 export default db;
