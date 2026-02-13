4d1397c6eccbdf02f18329bb1cfa4fc38ec5c93d
gowthamvetri <gowthamvetriii@gmail.com>
2026-02-13 21:23:16 +0530

    db problem fix-3

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -27,8 +27,25 @@ export const register = async(req,res)=>{
             error:true
         })
     } 
+    const password = name.substring(0,4)+phone.substring(0,4)
 
-    const hash = await bcrypt.hash(name.substring(0,4)+phone.substring(0,4),10)
+    const data = {
+                to:email,
+                subject:"Mail to inform that a new user is Created",
+                html:`
+                    <div style="font-family: sans-serif; border: 1px solid #c3e6cb; background-color: #d4edda; color: #155724; padding: 15px; border-radius: 5px; max-width: 400px; margin: 20px auto; text-align: center;">
+                        <h1>Hello, ${name}!</h1>
+                        <p>Welcome to our service. Your account has been successfully created.</p>
+                        <p>Please click the link below and use the credentials:</p>
+                        <p>Email:${email}</p>
+                        <p>Password:${password}</p>
+                        <a href=${'http://localhost:5173/'}>Confirm Account</a>
+                        <p>Thank you!</p>
+                    </div>`
+            }
+            sendMail(data)
+
+    const hash = await bcrypt.hash(password,10)
 
     const auth = new Auth({
             email:email,
